a515a8539abeadbd7f1924e71c77437107dca3f0
Rodrigo <roduartekatcher@gmail.com>
2019-11-12 22:24:57 -0300

    Revert "Seguimos dandole matraca"
    This reverts commit 5db5b7b595e2410a9adf00754cbb6838711f6a0e.

---
+++ CourseSimulationSystem/Entities/File.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class File
+    {
+        public StudentCourse StudentCourse { get; set; }
+        public String Name { get; set; }
+        public int Grade { get; set; }
+        public string FileSource { get; set; }
+    }
+}
---
+++ CourseSimulationSystem/Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Student
+    {
+        public int StudentNum { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public bool Logged { get; set; }
+
+        public Student()
+        {
+        }
+    }
+}
